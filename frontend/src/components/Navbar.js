7e2469ece70565d4e2d110943ad90b463c0e2cf1
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2024-01-03 19:16:19 +0700

    fix bugs in navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [scrollPosition, setScrollPosition] = useState(0)
 
   const ToggleMenu = () => {
       setIsMenuOpen(!isMenuOpen)
     }
 
+  // use effect digunakan untuk mengontrol efek perubahan dari scroll
+  useEffect(() => {
+    // buat closure
+    const handleScroll = () => {
+      const position = window.scrollY // var untuk menampung nilai scroll
+      setScrollPosition(position) // masukkan kedalam state
+    }
+
+    window.addEventListener('scroll', handleScroll, {passive:true}) // panggil event scroll
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll) // remove event
+    }
+  })
+
   return (
-    <nav className='text-white bg-transparent text-2xl py-8 px-12 flex justify-between sticky top-0 md:py-6 md:px-20'>
+    <nav className={`text-white text-2xl py-8 px-12 flex justify-between sticky top-0 md:py-6 md:px-20 transition-all duration-100 ${scrollPosition > 20 ? 'bg-secondary' : 'bg-transparent'}`}>
         <a href='#' className='font-rubik md:text-4xl'>Satrio</a>
 
         <ul className='hidden text-lg font-poppins text-gray-200 w-1/2 justify-between items-center md:flex lg:w-2/5'>
